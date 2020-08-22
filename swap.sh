42d46fd89d4f7a3fda8865e6a39ede43fedcb01f
Moexin <moexin@foxmail.com>
2020-08-23 01:43:36 +0800

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#Blog:https://moexin.top/post/linux-vps-yi-jian-tian-jia-shan-chu-swap-xu-ni-nei-cun
+#Blog:https://moexin.top/4.html
 
 Green="\033[32m"
 Font="\033[0m"
@@ -22,42 +22,42 @@ ovz_no(){
 }
 
 add_swap(){
-echo -e "${Green}请输入需要添加的swap，建议为内存的2倍！${Font}"
+echo -e "${Green}请输入需要添加的Swap，建议为内存的2倍！${Font}"
 read -p "请输入swap数值:" swapsize
 
-#检查是否存在swapfile
+#检查是否存在SwapFile
 grep -q "swapfile" /etc/fstab
 
-#如果不存在将为其创建swap
+#如果不存在将为其创建Swap
 if [ $? -ne 0 ]; then
-	echo -e "${Green}swapfile未发现，正在为其创建swapfile${Font}"
+	echo -e "${Green}SwapFile未发现，正在为其创建SwapFile${Font}"
 	fallocate -l ${swapsize}M /swapfile
 	chmod 600 /swapfile
 	mkswap /swapfile
 	swapon /swapfile
 	echo '/swapfile none swap defaults 0 0' >> /etc/fstab
-         echo -e "${Green}swap创建成功，并查看信息：${Font}"
+         echo -e "${Green}Swap创建成功，并查看信息：${Font}"
          cat /proc/swaps
          cat /proc/meminfo | grep Swap
 else
-	echo -e "${Red}swapfile已存在，swap设置失败，请先运行脚本删除swap后重新设置！${Font}"
+	echo -e "${Red}SwapFile已存在，Swap设置失败，请先运行脚本删除Swap后重新设置！${Font}"
 fi
 }
 
 del_swap(){
-#检查是否存在swapfile
+#检查是否存在SwapFile
 grep -q "swapfile" /etc/fstab
 
 #如果存在就将其移除
 if [ $? -eq 0 ]; then
-	echo -e "${Green}swapfile已发现，正在将其移除...${Font}"
+	echo -e "${Green}SwapFile已发现，正在将其移除...${Font}"
 	sed -i '/swapfile/d' /etc/fstab
 	echo "3" > /proc/sys/vm/drop_caches
 	swapoff -a
 	rm -f /swapfile
-    echo -e "${Green}swap已删除！${Font}"
+    echo -e "${Green}Swap已删除！${Font}"
 else
-	echo -e "${Red}swapfile未发现，swap删除失败！${Font}"
+	echo -e "${Red}SwapFile未发现，Swap删除失败！${Font}"
 fi
 }
 
@@ -67,9 +67,9 @@ root_need
 ovz_no
 clear
 echo -e "———————————————————————————————————————"
-echo -e "${Green}Linux VPS一键添加/删除swap脚本${Font}"
-echo -e "${Green}1、添加swap${Font}"
-echo -e "${Green}2、删除swap${Font}"
+echo -e "${Green}Linux VPS一键添加/删除Swap脚本${Font}"
+echo -e "${Green}1、添加Swap${Font}"
+echo -e "${Green}2、删除Swap${Font}"
 echo -e "———————————————————————————————————————"
 read -p "请输入数字 [1-2]:" num
 case "$num" in
