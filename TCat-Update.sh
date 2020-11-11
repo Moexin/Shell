ec575960542231bb7bfd699c662aa6a792106f1e
Moexin <moexin@foxmail.com>
2020-11-11 15:22:35 +0800

    rm

--- TCat-Update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-systemctl stop TCat
-cd /root/TCat
-rm -rf *
-wget https://github.com/tokumeikoi/aurora/releases/latest/download/aurora
-wget https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
-unzip v2ray-linux-64.zip
-chmod 755 *
-systemctl start TCat
